98a6787e5e990a77490a4acd4bb67733b5c0e7e9
TikhonovaA <a.tikhonova@g.nsu.ru>
2019-12-09 21:57:02 +0700

    add chart

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("Hello Fome developers");
+data = [{x: 10, y: 10}, {x: 100, y: 100},{x: 1000, y: 1000},{x: 10000, y: 10000},{x: 100000, y: 100000}];
+
+var scatterChartData = {
+    datasets: [{
+        pointBorderWidth: 0,
+        pointRadius: 5,
+        data: data
+    }]
+};
+
+var ctx = document.getElementById('myChart');
+var myScatter = new Chart(ctx, {
+    type: 'line',
+    data: scatterChartData,
+    options: {
+        scales: {
+            xAxes:[{
+                type: "logarithmic",
+                ticks: {
+                    min: 0,
+                }
+            }],
+        }
+    },
+});
