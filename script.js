22a33a366e0fcbf2a4c2dfc582e3fc967e12cdc8
TikhonovaA <atikhonova98@gmail.com>
2019-12-10 00:27:54 +0700

    add logscale y

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("Hello Fome developers");
-data = [{x: 10, y: 10}, {x: 100, y: 100},{x: 1000, y: 1000},{x: 10000, y: 10000},{x: 100000, y: 100000}];
+data = [{x: 10, y: 10}, {x: 100, y: 100},{x: 1000, y: 1000},{x: 10000, y: 10000},{x: 100000, y: 100000},{x: 0, y: 60000}];
 
 var scatterChartData = {
     datasets: [{
@@ -11,20 +11,58 @@ var scatterChartData = {
 
 window.onload = function() {
     var ctx = document.getElementById('myChart').getContext('2d');
-    ctx.canvas.height = 500;
-    ctx.canvas.width = 500;
+    ctx.canvas.height = 600;
+    ctx.canvas.width = 600;
     window.myScatter = new Chart(ctx, {
         type: 'line',
         data: scatterChartData,
         options: {
             scales: {
                 xAxes:[{
-                    type: "logarithmic",
+                    type: "linear",
                     ticks: {
                         min: 0,
+                    }
+                }],
+                yAxes:[{
+                    type: "linear",
+                    ticks: {
+                        min: 0,
+                        callback: function(value) {
+                            if (document.getElementById('logscale').value == 1) {
+                                if((value === 0) || (value === 1) || (value === 10) || (value === 100)) return value;
+                                else if (value%10 === 0) {
+                                    if(Math.log10(Number(value)) == Math.log10(Number(value)).toPrecision(1))
+                                        return  "10^" + Math.log10(Number(value)).toString();
+                                }
+                                else return "";
+                            }
+                            else return value;
+                        },
                     }
                 }],
             }
         },
     });
-}
+
+
+//set logscale y
+document.getElementById('logscale').addEventListener('click', function () {
+    let element = document.getElementById('logscale');
+    if(element.value == 0) {
+        window.myScatter.options.scales.yAxes[0].type = 'logarithmic';
+        element.value = 1;
+        window.myScatter.update();
+    }
+    else {
+        window.myScatter.options.scales.yAxes[0].type = 'linear';
+        element.value = 0;
+        window.myScatter.update();
+    }
+});
+
+
+
+
+
+};
