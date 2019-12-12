82cd7ad30ee72372ed728206959d98f4413c78c3
TikhonovaA <atikhonova98@gmail.com>
2019-12-12 16:19:31 +0700

    add constants and functions

--- script.js
+++ script.js
@@ -9,7 +9,69 @@ var scatterChartData = {
     }]
 };
 
+function getEffectiveDensityState(m, T){
+    return 2.51e19*Math.pow((m*T/(constatnt.m0*300.0)),1.5);
+}
+
+function getN(Nc, Eg, Ef, T){
+    return Nc*Math.exp((Ef-Eg)/(constatnt.k*T));
+}
+
+function getP(Nv, Ef, T){
+    return Nv*Math.exp((-Ef)/(constatnt.k*T));
+}
+
+function getNaMinus(Na0, Ea, Ef, T){
+    return Na0/(1+Math.exp((Ea-Ef)/(constatnt.k*T)));
+}
+
+function getNdPlus(Nd0, Ed, Ef, T){
+    return Nd0/(1+Math.exp((Ef-Ed)/(constatnt.k*T)));
+}
+
+function getConductivity(n, p, mue, mup){
+    return constatnt.e*(n*mue+p*mup)*100;
+}
+
+function func(Ef,Nc, Nv, T, Na0, Nd0, Eg, Ea, Ed){
+    let n = getN (Nc, Eg, Ef, T);
+    let p = getP (Nv, Ef, T);
+    let NdPlus = getNdPlus (T, Ed, Ef, Nd0);
+    let NaMinus = getNaMinus (T, Ea, Ef, Na0);
+    return NdPlus + p - n - NaMinus;
+}
+
+function getFermi(Nc, Nv, T, Na0, Nd0, Eg, Ea, Ed) {
+    let left = 0;
+    let right = 1e2;
+    let middle = (left + right) / 2.0;
+    let fm = func(middle,Nc,Nv,T,Na0,Nd0,Eg,Ea,Ed);
+    let iters = 0;
+    while (abs(fm) > 1 && iters < 1000) {
+        let fleft = func(left,Nc,Nv,T,Na0,Nd0,Eg,Ea,Ed);
+        let fright = func(right,Nc,Nv,T,Na0,Nd0,Eg,Ea,Ed);
+        if (fleft * fm < 0) {
+            right = middle;
+        } else if (fright * fm < 0) {
+            left = middle;
+        } else {
+            break;
+        }
+        middle = (left + right) / 2.0;
+        fm = func(middle,Nc,Nv,T,Na0,Nd0,Eg,Ea,Ed);
+        ++iters;
+    }
+    return middle;
+}
+
+function getMobility(a,  b, NdPlus, NaMinus, T){
+    return a/(Math.pow(T,1.5)+b*(NdPlus+NaMinus)/Math.pow(T,1.5));
+}
+
 window.onload = function() {
+
+
+
     var ctx = document.getElementById('myChart').getContext('2d');
     ctx.canvas.height = 600;
     ctx.canvas.width = 600;
