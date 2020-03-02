752a08245515aceeee58f663474cc7472a916850
TikhonovaA <47913089+TikhonovaA@users.noreply.github.com>
2020-03-02 17:50:30 +0700

    kvantograms

--- quantogram/spectr.js
+++ quantogram/spectr.js
@@ -671,7 +671,7 @@ function updateChart(new_atom, min, maxW){
     }
     if (letter.length > 0) {
         $.ajax({
-            url:"https://grotrian.nsu.ru/getdata.php",
+            url:"http://grotrian.nsu.ru/getdata.php",
             type: "post",
             dataType: "json",
             async: false,
